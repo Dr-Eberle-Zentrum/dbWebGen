c30bcca209f67988cff0dfd15022d776c74fb2f9
Michael Derntl <michael.derntl@uni-tuebingen.de>
2017-01-10 13:40:44 +0100

    allow HTML in table chart

--- inc/chart.table.php
+++ inc/chart.table.php
@@ -2,36 +2,44 @@
 	//==========================================================================================
 	class dbWebGenChart_table extends dbWebGenChart_Google {
 	//==========================================================================================
-		
+
 		//--------------------------------------------------------------------------------------
 		// form field @name must be prefixed with exact charttype followed by dash
-		public function settings_html() { 
+		public function settings_html() {
 		//--------------------------------------------------------------------------------------
 			return <<<HTML
 				<p>The query result will be visualized as a table.</p>
+				<div class="form-group">
+					<label class="control-label">Options</label>
+					<div class='checkbox top-margin-zero'>
+						<label>{$this->page->render_checkbox('table-allowHtml', 'ON', false)}Allow HTML inside cells</label>
+					</div>
+				</div>
 HTML;
 		}
-		
+
 		//--------------------------------------------------------------------------------------
 		// shall we subtract scrollbar from visualization width? default true
-		protected function shall_subtract_scrollbar() {			
+		protected function shall_subtract_scrollbar() {
 		//--------------------------------------------------------------------------------------
 			return false;
 		}
-		
+
 		//--------------------------------------------------------------------------------------
-		protected function options() {			
+		protected function options() {
 		//--------------------------------------------------------------------------------------
-			return parent::options();
+			return parent::options()  + array(
+				'allowHtml' => ($this->page->get_post('table-allowHtml') === 'ON')
+			);
 		}
-		
+
 		//--------------------------------------------------------------------------------------
 		// return google charts class name to instantiate
 		public function class_name() {
 		//--------------------------------------------------------------------------------------
 			return 'google.visualization.Table';
 		}
-		
+
 		//--------------------------------------------------------------------------------------
 		// return google charts packages to include
 		public function packages() {
