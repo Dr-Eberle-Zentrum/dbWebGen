202af1819e794f5d96c20b7a6185cffa9ded7753
Michael Derntl <michael.derntl@uni-tuebingen.de>
2017-04-28 14:00:27 +0200

    contextualized custom localization

--- inc/l10n.php
+++ inc/l10n.php
@@ -30,15 +30,23 @@
     }
 
     //--------------------------------------------------------------------------------------
-    // registers additional string entries (or overrides), useful for localized plugins.
+    // Registers additional string entries (or overrides) for some context, useful for localized plugins.
     // Plugins should call this once from their registered $APP['preprocess_func'] function
-    function l10n_register($lang, $string_table) {
+    function l10n_register(
+        $context,       // the unique caller context. for each session/context combo,
+                        // this function is only performed once, regardless how oftenn it is invoked
+                        // must not be empty!
+        $lang,          // language code (lower case), e.g. 'de'
+        $string_table   // array with key => localized string, can overwrite default keys
+    ) {
     //--------------------------------------------------------------------------------------
         if($lang != (defined('DBWEBGEN_LANG') ? DBWEBGEN_LANG : 'en'))
             return; // nothing to do
-        if(!isset($_SESSION['l10n_custom'])) {
-            $_SESSION['l10n_custom'] = true;
+        if(!isset($_SESSION['l10n_custom']))
+            $_SESSION['l10n_custom'] = array();
+        if(!isset($_SESSION['l10n_custom'][$context])) {
             $_SESSION['l10n'] = array_merge($_SESSION['l10n'], $string_table);
+            $_SESSION['l10n_custom'][$context] = true;
         }
     }
 ?>
