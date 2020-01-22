24c923b38f114d4c49365c94e0c40825f61b7b56
Michael Derntl <michael.derntl@uni-tuebingen.de>
2020-01-22 11:14:52 +0100

    setting to prevent remote delete of existing file during edit

--- inc/fields/field.upload.php
+++ inc/fields/field.upload.php
@@ -17,6 +17,14 @@
 		}
 
 		//--------------------------------------------------------------------------------------
+		// during edit mode, determines whether remote delete of existing file is allowed
+		public function allow_remote_delete() {
+		//--------------------------------------------------------------------------------------
+			return !isset($this->field['allow_remote_delete']) // default: true
+				|| $this->field['allow_remote_delete'] === true;
+		}
+
+		//--------------------------------------------------------------------------------------
 		protected function /*string*/ render_internal(&$output_buf) {
 		// render_settings: form_method, name_attr, id_attr
 		//--------------------------------------------------------------------------------------
@@ -32,7 +40,7 @@
 			);
 			if($_GET['mode'] == MODE_EDIT) {
 				$output_buf .= '<span class="help-block">' . l10n('upload-field.hint-empty') . '</span>';
-				if(!$this->is_required() && $this->get_submitted_value('') !== '') {
+				if(!$this->is_required() && $this->get_submitted_value('') !== '' && $this->allow_remote_delete()) {
 					// this control's attributes are used in dbweb.js in init_file_selection_handler() and in new_edit.php ---> be careful!!!
 					$output_buf .= sprintf(
 						'<span id="%s__remove_file_container" class="help-block top-margin-zero" ><input type="checkbox" value="remove" id="%s__remove_file" name="%s__remove_file"> %s</span>',
